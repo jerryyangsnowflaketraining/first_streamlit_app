722662aaffb9188cb626037258441ee71c3e5a2e
jerryyangsnowflaketraining <114030997+jerryyangsnowflaketraining@users.noreply.github.com>
2022-09-27 15:10:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,5 @@ if streamlit.button('Add a Fruit to the List'):
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
 
-streamlit.write('The user entered ', fruit_choice)
-
 # don't run anything past here while we troubleshoot
 streamlit.stop()
